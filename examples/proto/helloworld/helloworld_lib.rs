145200800f123a3efe72146ca8b3a332d2bcbd64
Colin Merkel <colin353@gmail.com>
2020-02-21 21:19:34 -0500

    clean up example

--- examples/proto/helloworld/helloworld_lib.rs
+++
@@ -1,5 +0,0 @@
-extern crate common;
-
-pub fn do_something(x: &helloworld_proto::HelloRequest) -> bool {
-    true
-}
--- examples/proto/helloworld/main.rs
+++
@@ -1,6 +0,0 @@
-extern crate helloworld_lib;
-extern crate helloworld_proto;
-
-pub fn main() {
-    helloworld_lib::do_something(&helloworld_proto::HelloRequest::new());
-}
